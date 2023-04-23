de541e59a534e92236fbe38826d0fd9e160eeb93
vinwinarta <16521380@std.stei.itb.ac.id>
2023-04-23 19:57:26 +0700

    Pekerjaan dan Inventory tolong cep

--- src/main/java/com/simplicity/Inventory.java
+++ src/main/java/com/simplicity/Inventory.java
@@ -1,5 +1,43 @@
 package com.simplicity;
 
+import java.util.ArrayList;
+import java.util.List;
+import javafx.util.Pair;
+
 public class Inventory {
-    public Inventory() {}
+    private List<Pair<Barang, Integer>> container;
+
+    public Inventory() {
+        container = new ArrayList<>();
+    }
+
+    public void displayBarang() {
+        for (Pair<Barang, Integer> item : container) {
+            System.out.println(item.getKey().getNama() + " : " + item.getValue());
+        }
+    }
+
+    public void addBarang(Barang barang, int jumlah) {
+        for (Pair<Barang, Integer> item : container) {
+            if (item.getKey().getNama().equals(barang.getNama())) {
+                item.setValue(item.getValue() + jumlah);
+                return;
+            }
+        }
+        container.add(new Pair<>(barang, jumlah));
+    }
+
+    public void reduceBarang(Barang barang, int jumlah) {
+        for (Pair<Barang, Integer> item : container) {
+            if (item.getKey().getNama().equals(barang.getNama())) {
+                int newJumlah = item.getValue() - jumlah;
+                if (newJumlah <= 0) {
+                    container.remove(item);
+                } else {
+                    item.setValue(newJumlah);
+                }
+                return;
+            }
+        }
+    }
 }
--- src/main/java/com/simplicity/Pekerjaan.java
+++ src/main/java/com/simplicity/Pekerjaan.java
@@ -1,5 +1,33 @@
 package com.simplicity;
 
-public class Pekerjaan {
-    public Pekerjaan() {}
+import java.util.*;
+public enum Pekerjaan {
+    BADUT_SULAP("Badut Sulap", 15),
+    KOKI("Koki", 30),
+    POLISI("Polisi", 35),
+    PROGRAMMER("Programmer", 45),
+    DOKTER("Dokter", 50);
+
+    private String nama;
+    private int gaji;
+
+    Pekerjaan(String nama, int gaji) {
+        this.nama = nama;
+        this.gaji = gaji;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getGaji() {
+        return gaji;
+    }
+
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+    public void setGaji(int gaji){
+        this.gaji=gaji;
+    }
 }
