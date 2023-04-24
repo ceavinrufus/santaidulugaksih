95ebafeafcebe076c7fd9eff2dab8b3d55fceac5
taraharyono <103413692+taraharyono@users.noreply.github.com>
2023-04-24 11:12:52 +0700

    Update Sim.java

--- src/main/java/com/simplicity/Sim.java
+++ src/main/java/com/simplicity/Sim.java
@@ -10,6 +10,7 @@ public class Sim {
     private Stats status = new Stats();
     private boolean isLibur = false;
     private Point currentLocation;
+    private Rumah rumah;
     private SimPosition currentPosition;
 
     public Sim(String namaLengkap, ArrayList<Pekerjaan> listPekerjaan) {
@@ -17,10 +18,19 @@ public class Sim {
         // Collections.shuffle(listPekerjaan);
         // this.pekerjaan = listPekerjaan.get(0);
         // currentLocation
-        Rumah rumah = new Rumah(this);
+        rumah = new Rumah(this);
         Ruangan ruang = rumah.getPeta().getElement(0, 0);
-        currentPosition = new SimPosition(rumah, ruang); 
+        currentPosition = new SimPosition(rumah, ruang);
     }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
 
     public void interact(Interactable barang){
         barang.aksi(this);
