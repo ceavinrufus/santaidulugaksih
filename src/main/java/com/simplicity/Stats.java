ae4263a3b1feaf644efbd28acbbb9a62fbfaa13d
Imanuel Raditya <16521421@mahasiswa.itb.ac.id>
2023-04-24 18:20:16 +0700

    Nambahin stats

--- src/main/java/com/simplicity/Stats.java
+++ src/main/java/com/simplicity/Stats.java
@@ -1,6 +1,61 @@
 package com.simplicity;
 
 public class Stats {
+    private int mood;
+    private int kekenyangan;
+    private int kesehatan;
+
     public Stats() {
+        mood = 80;
+        kekenyangan = 80;
+        kesehatan = 80;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public int getKekenyangan() {
+        return kekenyangan;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public void setKekenyangan(int kekenyangan) {
+        this.kekenyangan = kekenyangan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void tambahMood(int mood) {
+        this.mood += mood;
+    }
+
+    public void tambahKekenyangan(int kekenyangan) {
+        this.kekenyangan += kekenyangan;
+    }
+
+    public void tambahKesehatan(int kesehatan) {
+        this.kesehatan += kesehatan;
+    }
+
+    public void kurangMood(int mood) {
+        this.mood -= mood;
+    }
+
+    public void kurangKekenyangan(int kekenyangan) {
+        this.kekenyangan -= kekenyangan;
+    }
+
+    public void kurangKesehatan(int kesehatan) {
+        this.kesehatan -= kesehatan;
     }
 }
