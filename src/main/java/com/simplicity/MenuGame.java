f74ced42ea20cfdc3d064c5e56c314c489bc2668
christinaa21 <16521224@mahasiswa.itb.ac.id>
2023-04-25 21:56:59 +0700

    Created MenuGame.java

---
+++ src/main/java/com/simplicity/MenuGame.java
@@ -0,0 +1,85 @@
+package com.simplicity;
+
+import java.util.ArrayList;
+
+public class MenuGame {
+    private boolean isStarted;
+    private Sim currentSim;
+    private ArrayList<Pekerjaan> daftarPekerjaan;
+
+    public boolean getIsStarted() {
+        return isStarted;
+    }
+
+    public Sim getCurrentSim() {
+        return currentSim;
+    }
+
+    public ArrayList<Pekerjaan> getDaftarPekerjaan(){
+        return daftarPekerjaan;
+    }
+
+    public void setIsStarted(boolean isStarted){
+        this.isStarted = isStarted;
+    }
+
+    public void setCurrentSim(Sim currentSim){
+        this.currentSim = currentSim;
+    }
+
+    public void startGame(){
+        //
+    }
+
+    public void help(){
+        //
+    }
+
+    public void exit(){
+        //
+    }
+
+    public void viewSimInfo(){
+        //
+    }
+
+    public void addSim(){
+        //
+    }
+
+    public void changeSim(){
+        //
+    }
+
+    public void viewCurrentLocation(){
+        //
+    }
+
+    public void viewInventory(){
+        //
+    }
+
+    public void upgradeHouse(){
+        //
+    }
+
+    public void moveRoom(){
+        //
+    }
+
+    public void editRoom(){
+        //
+    }
+
+    public void displayBeforeGame(){
+        //
+    }
+
+    public void displayGame(){
+        //
+    }
+
+    public void displayAksiBarang(){
+        //
+    }
+}
