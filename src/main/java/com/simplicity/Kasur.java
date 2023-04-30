ba7291912b4396b5f08458d6a3a6804f32b36c05
taraharyono <103413692+taraharyono@users.noreply.github.com>
2023-04-30 17:00:59 +0700

    Update Jam, belom beres

--- src/main/java/com/simplicity/Kasur.java
+++ src/main/java/com/simplicity/Kasur.java
@@ -63,6 +63,7 @@ public class Kasur extends Furniture {
             totalWaktu.addWaktu(sleepTime);
             sim.setWaktuTidakTidur(0);
             sim.setIsSehabisTidur(true);
+            sim.setIsSehabisMakan(false);
         }
     }
 }
