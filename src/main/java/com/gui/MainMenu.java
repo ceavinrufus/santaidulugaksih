04efd9feed5e2d1f2f5d35d33e34cc0765fe46a6
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-05-01 08:47:01 +0700

    button positioninhg

--- src/main/java/com/gui/MainMenu.java
+++ src/main/java/com/gui/MainMenu.java
@@ -16,7 +16,6 @@ public class MainMenu extends JFrame {
 
     PixelatedButton newGameButton;
     PixelatedButton loadGameButton;
-    PixelatedButton helpButton;
 
     public MainMenu() {
         setTitle("Sim-Plicity");
@@ -74,28 +73,6 @@ public class MainMenu extends JFrame {
             }
         });
 
-        // Help button
-        helpButton = new PixelatedButton("Help");
-        helpButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // Help
-                Object[] options = { "Aku mengerti!" };
-                JOptionPane.showOptionDialog(null,
-                        "Hai! Selamat datang di Simplycity!\n" +
-                                "Klik Enter untuk memulai game\n" +
-                                "Klik Spasi untuk melihat help\n" +
-                                "Klik w, a, s, d atau tombol panah untuk menggerakan Sim dalam permainan\n" +
-                                "Selamat bermain!",
-                        "Help",
-                        JOptionPane.DEFAULT_OPTION,
-                        JOptionPane.INFORMATION_MESSAGE,
-                        null,
-                        options,
-                        options[0]);
-            }
-        });
-
         updateButtonPosition();
         // Add component listener to update button position when JFrame is resized
         addComponentListener(new ComponentAdapter() {
@@ -107,7 +84,6 @@ public class MainMenu extends JFrame {
 
         backgroundLabel.add(newGameButton);
         backgroundLabel.add(loadGameButton);
-        backgroundLabel.add(helpButton);
         setVisible(true);
         setFocusable(true);
     }
@@ -119,12 +95,9 @@ public class MainMenu extends JFrame {
     private void updateButtonPosition() {
         int buttonWidth = 200;
         int buttonHeight = 50;
-        newGameButton.setBounds(getWidth() / 3 - buttonWidth / 2, 27 * getHeight() / 40, buttonWidth,
+        newGameButton.setBounds(getWidth() / 2 - buttonWidth - 10, 27 * getHeight() / 40, buttonWidth,
                 buttonHeight);
-        helpButton.setBounds(2 * getWidth() / 3 - buttonWidth / 2, 27 * getHeight() / 40, buttonWidth,
-                buttonHeight);
-        loadGameButton.setBounds(getWidth() / 2 - buttonWidth / 2, 27 * getHeight() /
-                40, buttonWidth,
+        loadGameButton.setBounds(getWidth() / 2 + 10, 27 * getHeight() / 40, buttonWidth,
                 buttonHeight);
     }
 }
